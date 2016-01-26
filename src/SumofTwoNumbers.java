4b7210ba375902112058eafb01afefff247e5e76
MukundPradhan <replyme.mmp@gmail.com>
2016-01-27 02:11:25 +0530

    Update

---
+++ src/SumofTwoNumbers.java
@@ -0,0 +1,39 @@
+public class SumofTwoNumbers{
+
+  
+  public static void main(String[] args) {
+   
+    // Variable Declaration
+    
+     int intNum, flttoint;
+   
+     float fltNum;
+   
+     int sum;
+        
+
+      
+    //Initialisation
+  
+      intNum=10;
+    
+      fltNum=10.5f;
+        
+ 
+    
+    //Typecasting
+    
+      flttoint=(int) fltNum;
+  
+    
+    //sum calculation
+ 
+       sum=intNum + flttoint;
+    
+
+    System.out.println("The sum of two number is : " + sum);
+    
+  
+  }
+
+} 
